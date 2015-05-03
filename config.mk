1117c67b81f3c73cf440bba214fed6e273148d0c
Petr Vaněk <arkamar@atlas.cz>
2015-05-03 23:50:32 +0200

    Clean makefile config

--- config.mk
+++ config.mk
@@ -7,12 +7,9 @@ VERSION = 0.0
 PREFIX = /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
-
 # includes and libs
-INCS = -I. -I/usr/include -I${X11INC}
-LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 -lasound
+#INCS = -I. -I/usr/include
+#LIBS = -L/usr/lib
 
 # flags
 CPPFLAGS = -DVERSION=\"${VERSION}\" -D_DEFAULT_SOURCE
